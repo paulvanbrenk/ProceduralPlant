29bc69f1c2924edc2e471846f79bc1fc9eedc244
Greg Weil <gregweil@pentavera.com>
2015-12-01 20:13:25 -0500

    Basic renderer to match viewport with random cube/light for now

--- src/main.js
+++ src/main.js
@@ -1,5 +1,38 @@
+var renderer, scene, camera;
+
 window.onload = function() {
-	var renderer = new THREE.WebGLRenderer();
+	renderer = new THREE.WebGLRenderer();
 	document.body.appendChild(renderer.domElement);
-	renderer.setSize(400, 300);
+	renderer.setClearColor("navy", 1);
+	
+	scene = new THREE.Scene();
+	
+	camera = new THREE.PerspectiveCamera(
+		35,  //Field of view
+		4/3, //Aspect ratio
+		0.1, //Near plane
+		1000 //Far plane
+	);
+	camera.position.set(-15, 10, 10);
+	camera.lookAt(scene.position);
+	
+	var geometry = new THREE.BoxGeometry(5, 5, 5);
+	var material = new THREE.MeshLambertMaterial({color: "red"});
+	var mesh = new THREE.Mesh(geometry, material);
+	scene.add(mesh);
+	
+	var light = new THREE.PointLight("white");
+	light.position.set(10, 0, 10);
+	scene.add(light);
+	
+	window.onresize = redraw;
+	window.onresize();
+}
+
+redraw = function() {
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = (window.innerWidth / window.innerHeight);
+	camera.updateProjectionMatrix();
+	
+	renderer.render(scene, camera);
 }
