f111b6fbdf3842f657236556dd833644f9fde262
Adam Kruchkow <krucha@rpi.edu>
2015-12-04 14:40:48 -0500

    Plant updates.  Branch scaling and alignment.

--- src/makePlant1.js
+++ src/makePlant1.js
@@ -5,7 +5,18 @@
 // Variables --- o
 
 var currentIndex = 0;
-var triangle = new THREE.Triangle(new THREE.Vector3(-1,0,-.5),new THREE.Vector3(0,0,.75),new THREE.Vector3(1,0,-.5));
+var debug = false;
+
+var TRUNK =25;
+var HEIGHT = 0.5;
+var SCALE = 2.0;
+var DECAY = 0.05;
+
+var triangle = new THREE.Triangle(
+  new THREE.Vector3(-1,0,-.5).multiplyScalar(SCALE),
+  new THREE.Vector3(0,0,.75).multiplyScalar(SCALE),
+  new THREE.Vector3(1,0,-.5).multiplyScalar(SCALE)
+);
 
 // Functions --- o
 
@@ -18,25 +29,26 @@ function tAlign(tri,vector) {
   
   // ... Align with vector ... //
   
-  newTri = new THREE.Triangle(tri.a.add(center),tri.b.add(center),tri.c.add(center));
+  newTri = new THREE.Triangle(newTri.a.add(center),newTri.b.add(center),newTri.c.add(center));
   return newTri;
 }
 
-// Scale a triangle by a value (relative to its center). [incomplete] 
+// Scale a triangle by a value (relative to its center).
 function tScale(tri,value) {
   var center = tri.midpoint();
-  var newTri = new THREE.Triangle(tri.a.sub(center),tri.b.sub(center),tri.c.sub(center));
-  
-  // ... Scale points ... //
-  
-  newTri = new THREE.Triangle(tri.a.add(center),tri.b.add(center),tri.c.add(center));
-  return newTri;
+  var temp = tri.clone();
+  var d1 = tri.a.sub((temp.a.sub(center)).multiplyScalar(value));
+  var d2 = tri.b.sub((temp.b.sub(center)).multiplyScalar(value));
+  var d3 = tri.c.sub((temp.c.sub(center)).multiplyScalar(value));
+  return new THREE.Triangle(d1,d2,d3);
 }
 
 // Append a triangle to an existing vertex mesh
 function addTriangle(geometry,offset,tri,indices) {
 
-  var newTri = tAlign(tri,offset);
+  var newTri;
+  newTri = tScale(tri,DECAY);
+  // newTri = tAlign(newTri,offset);
   newTri = new THREE.Triangle( tri.a.add(offset), tri.b.add(offset), tri.c.add(offset) );
 
   geometry.vertices.push (
@@ -65,12 +77,27 @@ function addTriangle(geometry,offset,tri,indices) {
   return newTri;
 }
 
+function branch(geometry,offset,tri,indices,repeat) {
+  var T = tri.clone();
+  for (var i = 0; i < repeat; i++) {
+    T = addTriangle(geometry,new THREE.Vector3(0,HEIGHT*SCALE,0),tri,indices);
+    
+    // Branch out again!
+    if (T.area() > 1) {
+      var direction = T.plane().normalize().normal;
+      var points = new THREE.Vector3(currentIndex-1,currentIndex-4,currentIndex-5);
+      branch(geometry,direction,T,points,repeat/2)
+    }
+  
+  }
+}
+
 // Main function
 function plantMesh1(scene) {
 
   // Create base geometry and material
   var geometry = new THREE.Geometry();
-  var material = new THREE.MeshBasicMaterial({wireframe: true, color: "#553311"});
+  var material = new THREE.MeshBasicMaterial({wireframe: debug, color: "#553311"});
 
   // Create initial triangle
   var T = triangle.clone();
@@ -82,10 +109,13 @@ function plantMesh1(scene) {
   geometry.faces.push(new THREE.Face3(0,1,2));
   currentIndex += 3;
   
-  // Add triangles on top
-  for (var i = 0; i < 10; i++) {
-    T = addTriangle(geometry,new THREE.Vector3(0,.5,0),T,new THREE.Vector3(currentIndex-3,currentIndex-2,currentIndex-1));
+  // Trunk
+  for (var i = 0; i < TRUNK; i++) {
+    var direction = T.plane().normalize().normal;
+    addTriangle(geometry,direction,T,new THREE.Vector3(currentIndex-3,currentIndex-2,currentIndex-1));
   }
+  
+  // Branches
 
   // Return final mesh
   currentIndex = 0;
