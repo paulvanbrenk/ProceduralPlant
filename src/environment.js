0dbe847aa2392f77b51b7c41b7c64f6738d859ff
Greg Weil <weilg@rpi.edu>
2015-12-04 20:49:56 -0500

    Tweaked colors/fog, added a sky sphere

--- src/environment.js
+++ src/environment.js
@@ -19,11 +19,25 @@ function environment() {
 	planeGeo.verticesNeedUpdate = true;
 	planeGeo.computeFaceNormals();
 	planeGeo.computeVertexNormals();
-	var planeMat = new THREE.MeshPhongMaterial({color: "green"});
+	var planeMat = new THREE.MeshPhongMaterial({color: "#484"});
 	planeMat.shading = THREE.FlatShading;
 	var plane = new THREE.Mesh(planeGeo, planeMat);
 	plane.rotation.set(-Math.PI/2, 0, 0);
 	obj.add(plane);
+	
+	//Sky sphere
+	var skyGeo = new THREE.IcosahedronGeometry(15000, 4)
+	for (var i = 0; i < skyGeo.vertices.length; ++i) {
+		skyGeo.vertices[i].multiplyScalar(-1);
+	}
+	var skyMat = new THREE.MeshPhongMaterial({
+		shading: THREE.FlatShading,
+		color: "blue", fog: false});
+	skyMat.emissive = new THREE.Color("#447");
+	skyMat.shininess = 0;
+	var sky = new THREE.Mesh(skyGeo, skyMat);
+	sky.rotation.set(Math.random()*Math.PI, Math.random()*Math.PI, Math.random()*Math.PI);
+	obj.add(sky);
 	
 	return obj;
 }
