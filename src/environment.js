e25a8f7f1016e384956207b169a9d15d8b05bc3f
Greg Weil <weilg@rpi.edu>
2015-12-05 16:18:16 -0500

    Added a sort of procedural cloud

--- src/environment.js
+++ src/environment.js
@@ -45,5 +45,43 @@ function environment() {
 		sky.rotation.z += (0.000001 * dt);
 	});
 	
+	//Cloud
+	var cloudObj = new THREE.Object3D();
+	var cloudGeo = new THREE.PlaneGeometry(2, 2, 24, 24);
+	noise.seed(Math.random())
+	for (var i = 0; i < cloudGeo.vertices.length; ++i) {
+		var vert = cloudGeo.vertices[i];
+		var length = vert.length();
+		vert.z += (noise.perlin2(vert.x*2, vert.y*2) * 2);
+		vert.z -= (Math.pow(length, 2) * 2);
+	}
+	var cloudV = cloudGeo.vertices;
+	for (var i = 0; i < cloudGeo.faces.length; ++i) {
+		var face = cloudGeo.faces[i];
+		if (Math.min(cloudV[face.a].z, cloudV[face.b].z, cloudV[face.c].z) < -1) {
+			face.materialIndex = 1;
+		}
+	}
+	cloudGeo.verticesNeedUpdate = true;
+	cloudGeo.computeFaceNormals();
+	cloudGeo.computeVertexNormals();
+	var cloudMatVis = new THREE.MeshPhongMaterial({
+		shading: THREE.FlatShading, color: "#AAF", fog: false});
+	cloudMatVis.emissive = new THREE.Color("#AAA");
+	var cloudMatHid = new THREE.MeshLambertMaterial();
+	cloudMatHid.visible = false;
+	var cloudMat = new THREE.MeshFaceMaterial([cloudMatVis, cloudMatHid]);
+	var cloud = new THREE.Mesh(cloudGeo, cloudMat);
+	cloud.scale.set(2000, 1000, 30);
+	cloud.position.y = 3000;
+	cloud.position.z = -16000;
+	cloudObj.add(cloud);
+	obj.add(cloudObj);
+	
+	//Cloud rotation
+	updates.push(function(dt) {
+		cloudObj.rotation.y += (0.000005 * dt);
+	});
+	
 	return obj;
 }
